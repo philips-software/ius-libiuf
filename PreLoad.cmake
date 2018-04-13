49cb129b2f66ce905323aa6d1c8f27c6f33c8543
frank_v_heesch <frank.van.heesch@gmail.com>
2018-04-13 09:28:52 +0200

    IUS-99: compiler preference swapped

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -1,7 +1,8 @@
+if (NOT "$ENV{VS140COMNTOOLS}" STREQUAL "")
+    set(CMAKE_GENERATOR "Visual Studio 14 2015 Win64" CACHE INTERNAL "Name of generator.")
+endif()
+
 if (NOT "$ENV{VS150COMNTOOLS}" STREQUAL "")
     set(CMAKE_GENERATOR "Visual Studio 15 2017 Win64" CACHE INTERNAL "Name of generator.")
 endif()
 
-if (NOT "$ENV{VS140COMNTOOLS}" STREQUAL "")
-    set(CMAKE_GENERATOR "Visual Studio 14 2015 Win64" CACHE INTERNAL "Name of generator.")
-endif()
