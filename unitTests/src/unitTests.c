47b551334131e0da19c65f42581a747322165c95
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2018-08-10 11:25:48 +0200

    IUS-123: Added iusHLReceiveSettingsDict ADT

--- unitTests/src/unitTests.c
+++ unitTests/src/unitTests.c
@@ -39,6 +39,7 @@ static void RunAllTests(void)
     RUN_TEST_GROUP(IusPulse);
     RUN_TEST_GROUP(IusPulseDict);
     RUN_TEST_GROUP(IusReceiveSettings);
+    RUN_TEST_GROUP(IusReceiveSettingsDict);
     RUN_TEST_GROUP(IusReceiveChannelMap);
     RUN_TEST_GROUP(IusReceiveChannelMapDict);
     RUN_TEST_GROUP(IusTransducer);
