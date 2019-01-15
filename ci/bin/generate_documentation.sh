b32c0e66d541c13cbdae142716cade7b042f6f0a
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2019-01-15 10:11:04 +0100

    IUS-153: added GitLab pages step

---
+++ ci/bin/generate_documentation.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+function releaseNumber
+{
+    # tag found? use that
+    CommitHash=$(git log -1 --format=%h)
+    GitTag=$(git describe --tags --abbrev=0 --exact-match 2> /dev/null)
+    if (( $? == 0 ))
+    then
+        # otherwise use commit hash
+        Release="3.${GitTag}"
+    else
+        # otherwise use commit hash
+        Release="3.${CommitHash}"
+    fi
+    echo -n "$Release"
+}
+
+ScriptPath=$(dirname $(realpath $0))
+BuildFolder=$(realpath ${ScriptPath}/../..)/build
+StartFolder=$(pwd)
+IUSRelease="IUS-SDK-$(releaseNumber)"
+DocFolder="${StartFolder}/public/${IUSRelease}/documentation"
+DocSource="${StartFolder}/dox"
+
+echo === Generating documentation
+
+export PROJECT_NUMBER=$(releaseNumber)
+mkdir -p ${DocFolder}
+cd ${DocSource}
+doxygen 2>&1 | grep -iv Warning
+if [[ -d html ]]
+then
+    mv html ${DocFolder}
+    cp -r img ${DocFolder}
+fi
+echo === Done
