96ae5faf433a0820566415dc86e0adf3f907710f
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2019-01-24 11:50:04 +0100

    IUS-148: finished error handling for Ius3DParametricSource ADT

--- unitTests/src/unitTests.c
+++ unitTests/src/unitTests.c
@@ -14,7 +14,7 @@ static void RunAllTests(void)
 //    RUN_TEST_GROUP(Ius2DTransducerElement);
 //    RUN_TEST_GROUP(Ius2DTransducerElementList);
 //    RUN_TEST_GROUP(Ius3DNonParametricSource);
-//    RUN_TEST_GROUP(Ius3DParametricSource);
+    RUN_TEST_GROUP(Ius3DParametricSource);
     RUN_TEST_GROUP(Ius3DTransducer);
     RUN_TEST_GROUP(Ius3DTransducerElement);
     RUN_TEST_GROUP(Ius3DTransducerElementList);
