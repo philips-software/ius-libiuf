3c608752745750383caa01ebcc1e5683c2dcd94b
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2019-01-29 10:29:20 +0100

    IUS-157: Added iusPatternListDictGetKeys + test

--- library/src/iusPatternListDict.c
+++ library/src/iusPatternListDict.c
@@ -22,6 +22,7 @@ struct IusPatternListDict
 {
 	struct hashmap map;
 	IUS_BOOL deepDelete;
+    char **keys;
 };
 
 /* Declare type-specific blob_hashmap_* functions with this handy macro */
@@ -36,6 +37,7 @@ iupald_t iusPatternListDictCreate
 	IUS_ERR_ALLOC_NULL_N_RETURN(dict, IusPatternListDict, IUPALD_INVALID);
 	hashmap_init(&dict->map, hashmap_hash_string, hashmap_compare_string, 0);
 	dict->deepDelete = IUS_FALSE;
+    dict->keys = NULL;
 	return dict;
 }
 
@@ -49,6 +51,15 @@ int iusPatternListDictDeepDelete
 	IUS_ERR_CHECK_NULL_N_RETURN(dict, IUS_ERR_VALUE);
     dict->deepDelete = IUS_TRUE;
     return iusPatternListDictDelete(dict);
+}
+
+static void iusPatternListDictDeleteKeys
+(
+    iupald_t dict
+)
+{
+    if (dict->keys != NULL)
+        free(dict->keys);
 }
 
 int iusPatternListDictDelete
@@ -69,6 +80,7 @@ int iusPatternListDictDelete
         free(iterElement);
     }
 	hashmap_destroy(&dict->map);
+    iusPatternListDictDeleteKeys(dict);
 	free(dict);
 	return IUS_E_OK;
 }
@@ -124,12 +136,12 @@ int iusPatternListDictCompare
 }
 
 
-int iusPatternListDictGetSize
+size_t iusPatternListDictGetSize
 (
 	iupald_t dict
 )
 {
-	IUS_ERR_CHECK_NULL_N_RETURN(dict, -1);
+	IUS_ERR_CHECK_NULL_N_RETURN(dict, (size_t) -1);
 	return (int)hashmap_size(&dict->map);
 }
 
@@ -152,6 +164,40 @@ iupal_t iusPatternListDictGet
 	return search->patternList;
 }
 
+char **iusPatternListDictGetKeys
+(
+    iupald_t dict
+)
+{
+    IUS_ERR_CHECK_NULL_N_RETURN(dict, NULL);
+    return dict->keys;
+}
+
+static int iusPatternListDictUpdateKeys
+(
+    iupald_t dict
+)
+{
+    iusPatternListDictDeleteKeys(dict);
+    // allocate memory for the keys
+    int keyIndex;
+    size_t size = iusPatternListDictGetSize(dict);
+    dict->keys = calloc(size+1, sizeof(char*));
+    IUS_ERR_ALLOC_NULL_N_RETURN(dict, char *, IUS_ERR_VALUE);
+
+    struct hashmap_iter *iter;
+    HashablePatternList *iterElement;
+    IUS_ERR_CHECK_NULL_N_RETURN(dict, IUS_ERR_VALUE);
+    /* Free all allocated resources associated with map and reset its state */
+    for (iter = hashmap_iter(&dict->map), keyIndex=0; iter; iter = hashmap_iter_next(&dict->map, iter), keyIndex++)
+    {
+        iterElement = HashablePatternList_hashmap_iter_get_data(iter);
+        dict->keys[keyIndex] = iterElement->key;
+    }
+    dict->keys[keyIndex] = NULL;
+    return IUS_E_OK;
+}
+
 int iusPatternListDictSet
 (
 	iupald_t dict,
@@ -170,7 +216,7 @@ int iusPatternListDictSet
 		free(newMember);
 		return IUS_ERR_VALUE;
 	}
-	return IUS_E_OK;
+	return iusPatternListDictUpdateKeys(dict);
 }
 
 
