a4d812591efe506109f5bee763f0ee178e119a1f
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2019-02-05 08:01:47 +0100

    IUS-160: Disabled Debug build for windows dist

--- ci/bin/mk_os_distribution.bat
+++ ci/bin/mk_os_distribution.bat
@@ -8,8 +8,8 @@ echo === Building ius in %BuildFolder%
 set PlatformFolder=%BuildFolder%\Windows
 mkdir %PlatformFolder% & pushd %PlatformFolder%
 cmake ..\..
-cmake --build . --target install --config Debug
-if errorlevel 1 exit /B 1
+:: cmake --build . --target install --config Debug
+:: if errorlevel 1 exit /B 1
 cmake --build . --target install --config Release
 if errorlevel 1 exit /B 1
 popd
