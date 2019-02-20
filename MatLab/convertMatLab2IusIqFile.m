007417a3179db8d3f044ec9ef6dcecf6627f8100
310040622 <brian.bloemendal@philips.com>
2019-02-20 17:43:59 +0100

    Commented out old code; Now using the build version instead of the dist version of the python library

--- MatLab/convertMatLab2IusIqFile.m
+++ MatLab/convertMatLab2IusIqFile.m
@@ -117,96 +117,96 @@ iusTransmitApodizationDictDelete( transmitApodizationDict );
 
 end
 
-function convert( h, iusIqStruct, iqData, mode )
-
-% Get ius functions directly into local namespace
-import py.Python3Ius.*
-
-% sourceDict
-% FIXME: correct interpretation of numLocations!
-if iusIqStruct.DrivingScheme.drivingSchemeType == 3 % Single element!
-    SOURCE_LABEL = 'single_element';
-    numLocations = iusIqStruct.DrivingScheme.numTransmitSources;
-    if numLocations ~= 1
-        error('Single element recording expected.');
-    end
-    source = ius2DParametricSourceCreate( numLocations, 0, 0, 0);
-else
-    error('Driving scheme type not implemented yet');
-end
-sourceDict = iusSourceDictCreate();
-if 0 ~= iusSourceDictSet( sourceDict, SOURCE_LABEL, source)
-    iusErrorPrint();
-end
-if 0 ~= iusIqFileSetSourceDict( h, sourceDict )
-    iusErrorPrint();
-end
-
-% pulseDict
-PULSE_LABEL = 'doppler';
-if iusIqStruct.DrivingScheme.TransmitPulse.numPulseValues == 0
-    pulseFrequency = iusIqStruct.DrivingScheme.TransmitPulse.pulseFrequency;
-    pulseAmplitude = iusIqStruct.DrivingScheme.TransmitPulse.pulseAmplitude;
-    numPulses = iusIqStruct.DrivingScheme.TransmitPulse.pulseCount;
-    pulse = iusParametricPulseCreate( pulseFrequency, pulseAmplitude, numPulses );
-else
-    assert('Non parametric pulse not implemented yet!');
-end
-pulseDict = iusPulseDictCreate();
-if 0 ~= iusPulseDictSet( pulseDict, PULSE_LABEL, pulse )
-    iusErrorPrint();
-end
-if 0 ~= iusIqFileSetPulseDict( h, pulseDict )
-    iusErrorPrint();
-end
-
-% receiveSettingsDict
-%if 0 ~= iusIqFileSetReceiveSettingsDict(
-
-% receiveChannelMapDict ==> Demodulation scheme!
-
-% patternListDict
-
-% transmitApodizationDict
-% Convert from many TA's to a dict!
-TRANSMIT_APODIZATION_LABEL = 'doppler';
-ta = iusIqStruct.DrivingScheme.transmitApodization;
-transmitApodization = iusTransmitApodizationCreate( ta, size(ta, 2) );
-% for c1 = 1:size(ta, 2)
-%     iusTransmitApodizationSetElement( transmitApodization, c1-1, ta(1, c1) );
-% end
-iusTransmitApodizationSetApodization( transmitApodization, ta(1,:) );
-transmitApodizationDict = iusTransmitApodizationDictCreate();
-if 0 ~= iusTransmitApodizationDictSet( transmitApodizationDict, TRANSMIT_APODIZATION_LABEL, transmitApodization );
-    iusErrorPrint();
-end
-if 0 ~= iusIqFileSetTransmitApodizationDict( h, transmitApodizationDict )
-    iusErrorPrint();
-end
-
-% acquisition
-speedOfSound = iusIqStruct.Experiment.speedOfSound;
-date = int32(iusIqStruct.Experiment.date);
-description = iusIqStruct.Experiment.description;
-
-acquisition = iusAcquisitionCreate( speedOfSound, date, description);
-iusIqFileSetAcquisition( h, acquisition );
-
-% transducer
-
-
-% frameList
-
-
-% Store to disk
-iusIqFileNodeSave(h);
-
-% Free all elements
-iusAcquisitionDelete( acquisition );
-iusTransmitApodizationDelete( transmitApodization );
-
-
-end
+% % % % % function convert( h, iusIqStruct, iqData, mode )
+% % % % % 
+% % % % % % Get ius functions directly into local namespace
+% % % % % import py.Python3Ius.*
+% % % % % 
+% % % % % % sourceDict
+% % % % % % FIXME: correct interpretation of numLocations!
+% % % % % if iusIqStruct.DrivingScheme.drivingSchemeType == 3 % Single element!
+% % % % %     SOURCE_LABEL = 'single_element';
+% % % % %     numLocations = iusIqStruct.DrivingScheme.numTransmitSources;
+% % % % %     if numLocations ~= 1
+% % % % %         error('Single element recording expected.');
+% % % % %     end
+% % % % %     source = ius2DParametricSourceCreate( numLocations, 0, 0, 0);
+% % % % % else
+% % % % %     error('Driving scheme type not implemented yet');
+% % % % % end
+% % % % % sourceDict = iusSourceDictCreate();
+% % % % % if 0 ~= iusSourceDictSet( sourceDict, SOURCE_LABEL, source)
+% % % % %     iusErrorPrint();
+% % % % % end
+% % % % % if 0 ~= iusIqFileSetSourceDict( h, sourceDict )
+% % % % %     iusErrorPrint();
+% % % % % end
+% % % % % 
+% % % % % % pulseDict
+% % % % % PULSE_LABEL = 'doppler';
+% % % % % if iusIqStruct.DrivingScheme.TransmitPulse.numPulseValues == 0
+% % % % %     pulseFrequency = iusIqStruct.DrivingScheme.TransmitPulse.pulseFrequency;
+% % % % %     pulseAmplitude = iusIqStruct.DrivingScheme.TransmitPulse.pulseAmplitude;
+% % % % %     numPulses = iusIqStruct.DrivingScheme.TransmitPulse.pulseCount;
+% % % % %     pulse = iusParametricPulseCreate( pulseFrequency, pulseAmplitude, numPulses );
+% % % % % else
+% % % % %     assert('Non parametric pulse not implemented yet!');
+% % % % % end
+% % % % % pulseDict = iusPulseDictCreate();
+% % % % % if 0 ~= iusPulseDictSet( pulseDict, PULSE_LABEL, pulse )
+% % % % %     iusErrorPrint();
+% % % % % end
+% % % % % if 0 ~= iusIqFileSetPulseDict( h, pulseDict )
+% % % % %     iusErrorPrint();
+% % % % % end
+% % % % % 
+% % % % % % receiveSettingsDict
+% % % % % %if 0 ~= iusIqFileSetReceiveSettingsDict(
+% % % % % 
+% % % % % % receiveChannelMapDict ==> Demodulation scheme!
+% % % % % 
+% % % % % % patternListDict
+% % % % % 
+% % % % % % transmitApodizationDict
+% % % % % % Convert from many TA's to a dict!
+% % % % % TRANSMIT_APODIZATION_LABEL = 'doppler';
+% % % % % ta = iusIqStruct.DrivingScheme.transmitApodization;
+% % % % % transmitApodization = iusTransmitApodizationCreate( ta, size(ta, 2) );
+% % % % % % for c1 = 1:size(ta, 2)
+% % % % % %     iusTransmitApodizationSetElement( transmitApodization, c1-1, ta(1, c1) );
+% % % % % % end
+% % % % % iusTransmitApodizationSetApodization( transmitApodization, ta(1,:) );
+% % % % % transmitApodizationDict = iusTransmitApodizationDictCreate();
+% % % % % if 0 ~= iusTransmitApodizationDictSet( transmitApodizationDict, TRANSMIT_APODIZATION_LABEL, transmitApodization );
+% % % % %     iusErrorPrint();
+% % % % % end
+% % % % % if 0 ~= iusIqFileSetTransmitApodizationDict( h, transmitApodizationDict )
+% % % % %     iusErrorPrint();
+% % % % % end
+% % % % % 
+% % % % % % acquisition
+% % % % % speedOfSound = iusIqStruct.Experiment.speedOfSound;
+% % % % % date = int32(iusIqStruct.Experiment.date);
+% % % % % description = iusIqStruct.Experiment.description;
+% % % % % 
+% % % % % acquisition = iusAcquisitionCreate( speedOfSound, date, description);
+% % % % % iusIqFileSetAcquisition( h, acquisition );
+% % % % % 
+% % % % % % transducer
+% % % % % 
+% % % % % 
+% % % % % % frameList
+% % % % % 
+% % % % % 
+% % % % % % Store to disk
+% % % % % iusIqFileNodeSave(h);
+% % % % % 
+% % % % % % Free all elements
+% % % % % iusAcquisitionDelete( acquisition );
+% % % % % iusTransmitApodizationDelete( transmitApodization );
+% % % % % 
+% % % % % 
+% % % % % end
 
 function initIusLibrary()
 
@@ -223,11 +223,20 @@ end
 % Register python library wrapper to python search path (uses distribution
 % i.s.o. local build!
 if ispc
-    libpath = fullfile( pwd, '..', 'build', 'Windows', 'dist', 'python36', 'Windows');
+    %libpath = fullfile( pwd, '..', 'build', 'Windows', 'dist', 'python36', 'Windows');
+    libpath = fullfile( pwd, '..', 'build', 'Windows', 'python', 'python3');
+    if count(py.sys.path, libpath) == 0
+        insert(py.sys.path,int32(0),libpath);
+    end
+    libpath = fullfile( pwd, '..', 'build', 'Windows', 'python', 'python3', 'Release');
+    if count(py.sys.path, libpath) == 0
+        insert(py.sys.path,int32(0),libpath);
+    end
 else
-    libpath = fullfile( pwd, '..', 'build', 'Linux', 'dist', 'python36', 'Linux');
-end
-if count(py.sys.path, libpath) == 0
+    %libpath = fullfile( pwd, '..', 'build', 'Linux', 'dist', 'python36', 'Linux');
+    libpath = fullfile( pwd, '..', 'build', 'Linux', 'python', 'python3');
+    if count(py.sys.path, libpath) == 0
     insert(py.sys.path,int32(0),libpath);
+    end
 end
 end
