4b6e51ff699a9e59d0985029c793a72092b8cca2
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2019-03-20 17:22:17 +0100

    IUS-160: cmake should be used iso cmake3 for 3.14.0

--- ci/bin/mk_os_distribution.sh
+++ ci/bin/mk_os_distribution.sh
@@ -2,17 +2,20 @@
 ScriptPath=$(dirname $(realpath $0))
 BuildFolder=$(realpath ${ScriptPath}/../..)/build/$(uname)
 
-which cmake3 >/dev/null 2>&1
-if (( $? == 0 ))
-then
-    # cmake3 found
-    CMAKE=cmake3
-    CTEST=ctest3
-else
-    # use cmake
-    CMAKE=cmake
-    CTEST=ctest
-fi
+#which cmake3 >/dev/null 2>&1
+#if (( $? == 0 ))
+#then
+#    # cmake3 found
+#    CMAKE=cmake3
+#    CTEST=ctest3
+#else
+#    # use cmake
+#    CMAKE=cmake
+#    CTEST=ctest
+#fi
+
+CMAKE=cmake
+CTEST=ctest
 
 echo === Building iuf in $BuildFolder
 mkdir -p $BuildFolder
