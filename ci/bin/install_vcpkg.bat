dfe0c453fcb16ec5eb2a07090e28780302e4579f
Ruijzendaal <danny.ruijzendaal@philips.com>
2019-07-18 13:57:25 +0200

    IUS-236: IUS-236: WIP get travis build for windows up and running

---
+++ ci/bin/install_vcpkg.bat
@@ -0,0 +1,27 @@
+@echo off
+set DownloadFolder=%cd%\Download
+set VcpkgFolder=%DownloadFolder%\vcpkg
+
+pushd .
+
+:: Check if Download folder exists
+pushd %DownloadFolder% && popd || mkdir %DownloadFolder%
+cd %DownloadFolder%
+
+if not exist %VcpkgFolder% (
+    echo === Installing VCPKG..
+    git clone https://github.com/Microsoft/vcpkg.git
+    cd vcpkg
+    .\bootstrap-vcpkg.bat
+    echo Done
+) else (
+    echo  VCPKG has already been installed.
+    cd vcpkg
+)
+
+.\vcpkg install hdf5:x64-windows-static
+pause
+
+
+if errorlevel 1 exit /B 1
+popd
