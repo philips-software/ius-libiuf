9b687696a5442f68a329d8c71159a06e1eb0a5bf
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2019-07-22 13:10:03 +0200

    IUS-236: WIP get travis build for linux up and running

--- ci/bin/travis.sh
+++ ci/bin/travis.sh
@@ -16,22 +16,21 @@ function before_install_linux
                             software-properties-common \
                             tmate 
     printf "==> before_install_linux Done\n"
-    echo -e "\n\n\n" | ssh-keygen -t rsa
-}
+    echo  | ssh-keygen -t rsa -N ""
+ }
 
 
 function build_linux
 {
-    figho "Building Linux"
-    tmate
+    figho "Building....Linux.."
     ci/bin/build.sh
+    tmate
 }
 
 # Install some custom requirements on macOS
 # e.g. brew install pyenv-virtualenv
 case "${TRAVIS_OS_NAME}" in
     osx|linux)
-        rm -rf build
         eval $1_${TRAVIS_OS_NAME}
         ;;
     windows)
