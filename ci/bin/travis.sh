7919304c66aa81ab1d2c5f6fec4de73ae5919a8a
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2019-08-06 13:48:10 +0200

    IUS-241: WIP debugging OSX build

--- ci/bin/travis.sh
+++ ci/bin/travis.sh
@@ -21,7 +21,7 @@ function tmate_remote_debug
 # (It is using tmate.io tunneling services)
 #
 {
-    figho "TMATE Session.."
+    echo "TMATE Session.."
     echo  | ssh-keygen -t rsa -N ""
     tmate -S /tmp/tmate.sock new-session -d               # Launch tmate in a detached state
     tmate -S /tmp/tmate.sock wait tmate-ready             # Blocks until the SSH connection is established
@@ -36,7 +36,7 @@ function tmate_remote_debug
         fi
         sleep 2
     done
-    figho  "TMATE closed.."
+    echo  "TMATE closed.."
 }
 
 
