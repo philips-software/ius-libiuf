48e2b46dead50fdc4ebcf2dacea2593cc585f600
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2019-08-06 14:08:48 +0200

    IUS-241: WIP debugging OSX build

--- ci/bin/travis.sh
+++ ci/bin/travis.sh
@@ -174,13 +174,10 @@ function before_install_osx
     printf "==> before_install_osx\n"
     # Install tested version 3.14.0 of cmake
     #                1.10.5_1 of hdf5
-    tmate_remote_debug
+#    tmate_remote_debug
     brew unlink cmake
-    brew install tmate figlet \
-    https://raw.githubusercontent.com/Homebrew/homebrew-core/2f58eb8791f88dbf380523508a3a9aaf815f3976/Formula/cmake.rb \
-    https://raw.githubusercontent.com/Homebrew/homebrew-core/ac1d63eb675c3a963a363bf7ef0504024de81487/Formula/hdf5.rb
-    brew unlink cmake
-    brew link cmake
+    brew install figlet \
+    https://raw.githubusercontent.com/Homebrew/homebrew-core/2f58eb8791f88dbf380523508a3a9aaf815f3976/Formula/cmake.rb
     printf "==> before_install_osx Done\n"
     tmate_remote_debug
  }
